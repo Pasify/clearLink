b6b47306e96679fdc0cceac59743308e4982d5cd
pasify <pascalobiora73@gmail.com>
2024-01-24 16:53:50 +0100

    final draft

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
 "use strict";
 const accordionBtn = document.querySelectorAll(".accordion-button");
 const yearElement = document.querySelector(".year");
-
+const mobileMenu = document.querySelector("#mobileMenu");
+const nav = document.querySelector(".nav");
 accordionBtn.forEach((btn) =>
   btn.addEventListener("click", function () {
     let panel = btn.parentElement.nextElementSibling;
@@ -17,3 +18,15 @@ accordionBtn.forEach((btn) =>
 
 let currentYear = new Date().getFullYear();
 yearElement.textContent = currentYear;
+
+nav.addEventListener("click", function (e) {
+  let hamburger = e.target.closest("#hamburger");
+  if (!hamburger) return;
+  // mobileMenu.classList.toggle("hidden");
+
+  if (mobileMenu.classList.contains("hidden")) {
+    mobileMenu.classList.remove("hidden");
+  } else {
+    mobileMenu.classList.add("hidden");
+  }
+});
